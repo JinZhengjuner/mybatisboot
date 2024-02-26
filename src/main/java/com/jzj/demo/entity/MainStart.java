ea6d78890393ff412ca88cd3179a174d6c22b9a1
wb_jinzhengjun <wb_jinzhengjun@meituan.com>
2024-02-26 18:03:10 +0800

    模拟三级缓存

--- src/main/java/com/jzj/demo/entity/MainStart.java
+++ src/main/java/com/jzj/demo/entity/MainStart.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 
 import java.lang.reflect.Field;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MainStart {
@@ -38,20 +38,32 @@ public class MainStart {
     //二级缓存：为了将成熟Bean，和纯净Bean分离开来，避免读取到不完整的bean
     private static Map<String, Object> earlySingletonObject = new ConcurrentHashMap<>();
 
+    //三级缓存:为了解耦，代码更清晰
+    private static Map<String, ObjectFactory> singletonFactories = new ConcurrentHashMap<>();
+
+    //循环依赖的标识
+    private static Set<String> singletonCurrentlyCreating = new HashSet<>();
+
     //假设A 使用了Aop，要给A创建动态代理
     public static Object getBean(String beanName) throws Exception{
         Object singleton = getSingleton(beanName);
         if (singleton != null){
             return singleton;
         }
+
+        if (!singletonCurrentlyCreating.contains(beanName)){
+            singletonCurrentlyCreating.add(beanName);
+        }
         //实例化
         RootBeanDefinition beanDefinition = (RootBeanDefinition) beanDefinitionMap.get(beanName);
         Class<?> beanClass = beanDefinition.getBeanClass();
         Object instanceBean = beanClass.newInstance();
 
-        earlySingletonObject.put(beanName, instanceBean);
+        singletonFactories.put(beanName,() -> new JdkProxyBeanPostProcessor().getEarlyBeanReference(instanceBean, beanName));
 
-        instanceBean = new JdkProxyBeanPostProcessor().getEarlyBeanReference(instanceBean, beanName);
+        //这里二级缓存就不存了，万一对象还不是代理对象，到时候取到的就不是代理对象
+//        earlySingletonObject.put(beanName, instanceBean);
+
 
         //创建动态代理 （不是耦合的方式， beanPostProcessor）
         //添加到二级缓存，spring在初始化之后也调用了后置处理器生成动态代理
@@ -74,20 +86,29 @@ public class MainStart {
         }
         //初始化  inil.... initMethod @postConstract
         //添加到一级缓存  若在这里创建aop，已经晚了，A里面的B就不是动态代理
+        if (earlySingletonObject.containsKey(beanName)){
+            instanceBean = earlySingletonObject.get(beanName);
+        }
         singletonObjects.put(beanName, instanceBean);
         return instanceBean;
     }
 
     public static Object getSingleton(String beanName){
-        if (singletonObjects.containsKey(beanName)){
-            return singletonObjects.get(beanName);
-        }else if (earlySingletonObject.containsKey(beanName)){
-            //说明是循环依赖
-            Object obj = new JdkProxyBeanPostProcessor().getEarlyBeanReference(earlySingletonObject.get(beanName), beanName);
-            earlySingletonObject.put(beanName, obj);
-            return obj;
-//            return earlySingletonObject.get(beanName);
+        Object bean = singletonObjects.get(beanName);
+        //一级缓存没有，正在创建，就是循环依赖
+        if (bean ==null && singletonCurrentlyCreating.contains(beanName)){
+
+            //从二级缓存中去拿
+            if (earlySingletonObject.containsKey(beanName)){
+                return earlySingletonObject.get(beanName);
+            }
+            //从三级缓存拿
+            ObjectFactory factory = singletonFactories.get(beanName);
+            if (factory != null){
+                bean = factory.getObject();
+                earlySingletonObject.put(beanName, bean);
+            }
         }
-        return null;
+        return bean;
     }
 }
