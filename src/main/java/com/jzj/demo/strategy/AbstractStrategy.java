c162ce122778ab77f145900e8d22f437423108c9
wb_jinzhengjun <wb_jinzhengjun@meituan.com>
2024-06-03 15:54:35 +0800

    合并

--- src/main/java/com/jzj/demo/strategy/AbstractStrategy.java
+++ src/main/java/com/jzj/demo/strategy/AbstractStrategy.java
@@ -3,6 +3,9 @@ package com.jzj.demo.strategy;
 public abstract class AbstractStrategy implements Strategy{
     public void register(){
         StrategyContext.registerStrategy(getClass().getSimpleName(), this);
-        //
+        //1
+        //2
+        //3
+        //4
     }
 }
